da417a6610e840a205ce6125be4a8d54599db41c
Hao Liu <haoliu@thoughtworks.com>
2018-08-17 17:19:59 +0800

    Scanf & getcher

---
+++ ch3_data/main/platinum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+
+    float weight;
+    float value;
+
+    printf("Are you worth your weight in platinum? \n");
+    printf("Let's check it out. \n");
+    printf("please enter your weight in pounds: ");
+
+    scanf("%f",&weight);
+    value = 1700.0 * weight * 14.5833;
+
+    printf("getChar: %d \n", getchar());
+    printf("getChar: %d \n", getchar());
+    printf("Your weight in platinum is worth $%.2f. \n", value);
+    return 0;
+}

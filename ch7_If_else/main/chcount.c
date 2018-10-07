f0b371ef678392232429cec4a13389a197925014
Hao Liu <haoliu@thoughtworks.com>
2018-10-07 12:07:00 +0800

    Chapter 7 include continue, break, ctype.h

---
+++ ch7_If_else/main/chcount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define PERIOD '.'
+int main(void)
+{
+    char ch;
+    int charCount = 0;
+
+    while ((ch = getchar()) != PERIOD)
+    {
+        if (ch != '"' && ch != '\'')
+            charCount++;
+    }
+
+    printf("There are %d non-quote characters. \n", charCount);
+
+    return 0;
+}
